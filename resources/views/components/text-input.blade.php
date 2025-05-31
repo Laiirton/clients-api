14178b68c1d67a005ca8e62cfa3d3e32f9221afa
Lairton <130435095+Laiirton@users.noreply.github.com>
2025-05-31 19:16:39 -0300

    Update text-input.blade.php

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-zinc-800 dark:text-gray-300 focus:border-blue-500 dark:focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-zinc-600 dark:bg-zinc-800/80 dark:text-gray-100 focus:border-blue-500 dark:focus:border-blue-400 focus:ring-blue-500 dark:focus:ring-blue-400 rounded-md shadow-sm dark:placeholder-gray-400']) !!}>
