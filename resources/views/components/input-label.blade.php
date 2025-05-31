836fadbbf71491fd87fa8f7ea84b85c3323560a8
Lairton <130435095+Laiirton@users.noreply.github.com>
2025-05-31 19:16:41 -0300

    Update input-label.blade.php

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value', 'for'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }} for="{{ $for }}">
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-200']) }} for="{{ $for }}">
     {{ $value ?? $slot }}
 </label>
