5a390f7e6d1c5b9107f8a370f5d593afbd2e3f7f
Lairton <130435095+Laiirton@users.noreply.github.com>
2025-05-31 18:23:00 -0300

    Create text-input.blade.php

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,3 @@
+@props(['disabled' => false])
+
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-zinc-800 dark:text-gray-300 focus:border-blue-500 dark:focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500 rounded-md shadow-sm']) !!}>
